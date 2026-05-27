1a878c8a6dbff0361d9baa6a8097d40aaf69010e
Matheus-Tobias-Mustaro-10409259 <162312537+Matheus-Tobias-Mustaro-10409259@users.noreply.github.com>
2026-05-27 08:20:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-def agendar_consulta(paciente, medico):
-    if not paciente or not medico:
-        raise ValueError("Paciente e médico são obrigatórios")
-
-    return f"Consulta agendada para {paciente} com Dr(a). {medico}"
-
-
-if __name__ == "__main__":
-    print(agendar_consulta("João Silva", "Ana Souza"))
+def agendar_consulta(paciente, medico)
+    return "Erro proposital"
